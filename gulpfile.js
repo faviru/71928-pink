542d402356c5dfd44867aced618e48785433b6ae
Katerine <katerina0708@gmail.com>
2016-06-30 16:09:33 +0300

    Автоматизирована сборка личного проекта

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,38 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
+var mincss = require("gulp-csso");
+var rename = require("gulp-rename");
 var server = require("browser-sync");
+var imagemin = require("gulp-imagemin");
+var run = require("run-sequence");
+var del = require("del");
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png,jpg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true})
+  ]))
+  .pipe(gulp.dest("build/img"));
+});
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -18,10 +49,18 @@ gulp.task("style", function() {
         "last 2 Firefox versions",
         "last 2 Opera versions",
         "last 2 Edge versions"
-      ]})
+      ]}),
+      mqpacker({
+        sort: true
+      })
     ]))
+    .pipe(gulp.dest("build/css"))
     .pipe(gulp.dest("css"))
-    .pipe(server.reload({stream: true}));
+    .pipe(server.reload({stream: true}))
+    .pipe(mincss())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest("css"));
 });
 
 gulp.task("serve", ["style"], function() {
@@ -34,4 +73,14 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task("build", function(fn) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "images",
+    fn
+  );
 });
